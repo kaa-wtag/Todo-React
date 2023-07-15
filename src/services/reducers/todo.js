9f8cfb48dba758fea1894f0fbfb0967a8b86a8f2
Khaled Ahmmed Anik <khaled.ahmmed@welldev.io>
2023-07-16 01:47:12 +0600

    2 space tab and new line in todo.js

--- src/services/reducers/todo.js
+++ src/services/reducers/todo.js
@@ -1,43 +1,43 @@
 const initialTodos = {
-    todos: [],
+  todos: [],
 };
 
 const todoReducer = (state = initialTodos, action) => {
-    switch (action.type) {
-        case "ADD":
-            return {
-                ...state,
-                todos: [
-                    action.payload,
-                    ...state.todos,
-                ],
-            };
+  switch (action.type) {
+    case "ADD":
+      return {
+        ...state,
+        todos: [
+          action.payload,
+          ...state.todos,
+        ],
+      };
 
-        case "DELETE":
-            const updatedTodos = state.todos.filter((todo) => todo.id !== action.payload.id);
-            return {
-                ...state,
-                todos: updatedTodos,
-            };
+    case "DELETE":
+      const updatedTodos = state.todos.filter((todo) => todo.id !== action.payload.id);
+      return {
+        ...state,
+        todos: updatedTodos,
+      };
 
-        case "MARK_DONE":
-            const markedTodos = state.todos.map((todo) => {
-                if (todo.id === action.payload.id) {
-                    return {
-                        ...todo,
-                        isCompleted: true,
-                    };
-                }
-                return todo;
-            });
-            return {
-                ...state,
-                todos: markedTodos,
-            };
+    case "MARK_DONE":
+      const markedTodos = state.todos.map((todo) => {
+        if (todo.id === action.payload.id) {
+          return {
+            ...todo,
+            isCompleted: true,
+          };
+        }
+        return todo;
+      });
+      return {
+        ...state,
+        todos: markedTodos,
+      };
 
-        default:
-            return state;
-    }
+    default:
+      return state;
+  }
 };
 
 export default todoReducer;
