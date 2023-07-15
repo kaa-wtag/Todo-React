7218bdae3701005dc5925c1117265d5137848ca6
Khaled Ahmmed Anik <khaled.ahmmed@welldev.io>
2023-07-15 23:40:09 +0600

    change as 2 tab space and new line

--- src/Helpers/dateFormatehelpers.js
+++ src/Helpers/dateFormatehelpers.js
@@ -1,7 +1,7 @@
 export const formatDate = () => {
-    const today = new Date();
-    const day = String(today.getDate()).padStart(2, "0");
-    const month = String(today.getMonth() + 1).padStart(2, "0");
-    const year = today.getFullYear();
-    return `${day}.${month}.${year}`;
+  const today = new Date();
+  const day = String(today.getDate()).padStart(2, "0");
+  const month = String(today.getMonth() + 1).padStart(2, "0");
+  const year = today.getFullYear();
+  return `${day}.${month}.${year}\n`;
 };
