7cc8ce234913aa35ba54f33d320d7b041ff82821
KHALED AHMMED ANIK <khaled.ahmmed@welldev.io>
2023-07-18 02:04:09 +0600

    update action, reducer, todo.jsx for edit

--- src/services/actions/todo.js
+++ src/services/actions/todo.js
@@ -1,20 +1,30 @@
 export const addTodo = (props) => {
-  return {
-    type: "ADD",
-    payload: props,
-  };
+    return {
+        type: "ADD",
+        payload: props,
+    };
 };
 
 export const deleteTodo = (id) => {
-  return {
-    type: "DELETE_TODO",
-    payload: id,
-  };
+    return {
+        type: "DELETE_TODO",
+        payload: id,
+    };
 };
 
 export const markDone = (id) => {
-  return {
-    type: "MARK_DONE",
-    payload: id,
-  };
+    return {
+        type: "MARK_DONE",
+        payload: id,
+    };
+};
+
+export const editTodoText = (id, newText) => {
+    return {
+        type: "EDIT_TODO",
+        payload: {
+            id: id,
+            newText: newText,
+        },
+    };
 };
